9fcd17ea1a208ab28b73833165e69154c925cebb
aminbaks <44721247+aminbaks@users.noreply.github.com>
2019-10-04 02:04:33 +0330

    control activity layout just is down

--- app/src/main/java/com/amin/bluetoothmanitor/activity/ControlActivity.kt
+++ app/src/main/java/com/amin/bluetoothmanitor/activity/ControlActivity.kt
@@ -11,5 +11,6 @@ class ControlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
+
     }
 }
